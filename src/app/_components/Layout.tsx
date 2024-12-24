4a89b02bd31576b2389b000d51e44e201145b258
baadillahnabil <baadillahnabil@gmail.com>
2024-12-24 10:22:04 +0700

    feat: include sidebar and navbar in layout

--- src/app/_components/Layout.tsx
+++ src/app/_components/Layout.tsx
@@ -2,8 +2,12 @@
 
 import { AntdRegistry } from "@ant-design/nextjs-registry";
 import { StyleProvider } from "@ant-design/cssinjs";
+import { Layout, ConfigProvider } from "antd";
 
 import Sidebar from "./Sidebar";
+import Navbar from "./Navbar";
+
+const { Content } = Layout;
 
 export default function MainLayout({
   children,
@@ -13,10 +17,33 @@ export default function MainLayout({
   return (
     <AntdRegistry>
       <StyleProvider layer>
-        <section className="flex">
-          <Sidebar />
-          <main className="border border-green-800">{children}</main>
-        </section>
+        <ConfigProvider
+          theme={{
+            token: {
+              colorBgLayout: "#F8F8F8",
+              colorPrimary: "#1890FF",
+            },
+          }}
+        >
+          <Layout hasSider className="min-h-screen">
+            <Sidebar />
+            <Layout className="mt-5 pt-5">
+              <Navbar />
+              <Content
+                className="min-h-screen p-6 mx-4 my-6"
+                // style={{
+                //   margin: "24px 16px",
+                //   padding: 24,
+                //   minHeight: "100vh",
+                //   background: colorBgContainer,
+                //   borderRadius: borderRadiusLG,
+                // }}
+              >
+                {children}
+              </Content>
+            </Layout>
+          </Layout>
+        </ConfigProvider>
       </StyleProvider>
     </AntdRegistry>
   );
