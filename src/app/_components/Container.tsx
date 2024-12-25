5640d410c51c14fbd254bbff3f4acc47181df138
baadillahnabil <baadillahnabil@gmail.com>
2024-12-25 22:09:03 +0700

    feat: enhance Container component to accept additional props and className

--- src/app/_components/Container.tsx
+++ src/app/_components/Container.tsx
@@ -2,19 +2,25 @@
 
 import { theme } from "antd";
 
+type ContainerProps = React.HTMLAttributes<HTMLDivElement> &
+  React.PropsWithChildren & {
+    children: React.ReactNode;
+  };
+
 export default function Container({
   children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+  className,
+  ...rest
+}: Readonly<ContainerProps>) {
   const {
     token: { colorBgContainer },
   } = theme.useToken();
 
   return (
     <section
-      className="rounded-2xl p-4 shadow-sm"
+      className={`rounded-2xl p-4 shadow-sm ${className}`}
       style={{ backgroundColor: colorBgContainer }}
+      {...rest}
     >
       {children}
     </section>
