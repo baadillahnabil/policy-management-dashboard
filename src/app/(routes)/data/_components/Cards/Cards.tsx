241cbc71780e6c69b6db2b91ed916068c46266f9
baadillahnabil <baadillahnabil@gmail.com>
2024-12-25 16:01:22 +0700

    feat: add empty state handling in Cards component

--- src/app/(routes)/data/_components/Cards/Cards.tsx
+++ src/app/(routes)/data/_components/Cards/Cards.tsx
@@ -1,15 +1,22 @@
-import { Card } from "antd";
+import { Card, Empty } from "antd";
 
-import { type PromiseReturnType } from "@prisma/client/extension";
-import { getData } from "@/app/(routes)/data/_actions/actions";
+import { type GetDataType } from "@/app/(routes)/data/_actions/actions";
 
 import CardBody from "@/app/(routes)/data/_components/Cards/CardBody";
 
 interface Cards {
-  data: PromiseReturnType<typeof getData>;
+  data: GetDataType;
 }
 
 const Cards = ({ data }: Cards) => {
+  if (data.length <= 0) {
+    return (
+      <section className="flex items-center justify-center my-10 border border-gray-200 rounded-lg py-10">
+        <Empty />
+      </section>
+    );
+  }
+
   return (
     <section className="grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5">
       {data.map((item) => (
