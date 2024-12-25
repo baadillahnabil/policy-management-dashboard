428cbfd7d945c7fafea3889db2c413c54f999fe6
baadillahnabil <baadillahnabil@gmail.com>
2024-12-25 16:34:36 +0700

    feat: implement popover for filter options in Filters component

--- src/app/(routes)/data/_components/Header/Filters.tsx
+++ src/app/(routes)/data/_components/Header/Filters.tsx
@@ -2,8 +2,16 @@
 
 import { useSearchParams } from "next/navigation";
 import { useRouter } from "next-nprogress-bar";
-import { Form, Select, DatePicker, type FormProps } from "antd";
+import {
+  Form,
+  Select,
+  DatePicker,
+  Popover,
+  Button,
+  type FormProps,
+} from "antd";
 import dayjs from "dayjs";
+import { FaAngleDown } from "react-icons/fa";
 
 import {
   GetLocationsType,
@@ -44,80 +52,100 @@ const Filters = ({ locations, topics, statuses }: FiltersProps) => {
   };
 
   return (
-    <Form
-      form={form}
-      initialValues={{
-        location:
-          Number(searchParams.get("location")) <= 0
-            ? undefined
-            : Number(searchParams.get("location")),
-        topic:
-          Number(searchParams.get("topic")) <= 0
-            ? undefined
-            : Number(searchParams.get("topic")),
-        status:
-          Number(searchParams.get("status")) <= 0
-            ? undefined
-            : Number(searchParams.get("status")),
-        date: [
-          searchParams.get("dateFrom")
-            ? dayjs(searchParams.get("dateFrom"))
-            : undefined,
-          searchParams.get("dateTo")
-            ? dayjs(searchParams.get("dateTo"))
-            : undefined,
-        ],
-      }}
-      layout="vertical"
-      onFinish={handleSubmit}
-      onValuesChange={handleFieldsChange}
-      autoComplete="off"
-      className="flex items-center gap-4 flex-wrap"
+    <Popover
+      title="Filters"
+      trigger="click"
+      placement="bottomRight"
+      content={
+        <Form
+          form={form}
+          initialValues={{
+            location:
+              Number(searchParams.get("location")) <= 0
+                ? undefined
+                : Number(searchParams.get("location")),
+            topic:
+              Number(searchParams.get("topic")) <= 0
+                ? undefined
+                : Number(searchParams.get("topic")),
+            status:
+              Number(searchParams.get("status")) <= 0
+                ? undefined
+                : Number(searchParams.get("status")),
+            date: [
+              searchParams.get("dateFrom")
+                ? dayjs(searchParams.get("dateFrom"))
+                : undefined,
+              searchParams.get("dateTo")
+                ? dayjs(searchParams.get("dateTo"))
+                : undefined,
+            ],
+          }}
+          layout="vertical"
+          onFinish={handleSubmit}
+          onValuesChange={handleFieldsChange}
+          autoComplete="off"
+          className="flex flex-col items-center gap-4 [&>div]:w-full"
+        >
+          <Form.Item
+            name="location"
+            label="Filter By Location:"
+            className="mb-0"
+          >
+            <Select
+              options={locations.map(({ id, name }) => ({
+                value: id,
+                label: name,
+              }))}
+              placeholder="Select a location"
+              allowClear
+              className="w-full"
+              size="small"
+            />
+          </Form.Item>
+          <Form.Item name="topic" label="Filter By Topic:" className="mb-0">
+            <Select
+              options={topics.map(({ id, name }) => ({
+                value: id,
+                label: name,
+              }))}
+              placeholder="Select a topic"
+              allowClear
+              className="w-full"
+              size="small"
+            />
+          </Form.Item>
+          <Form.Item name="status" label="Filter By Status:" className="mb-0">
+            <Select
+              options={statuses.map(({ id, name }) => ({
+                value: id,
+                label: name,
+              }))}
+              placeholder="Select a status"
+              allowClear
+              className="w-full"
+              size="small"
+            />
+          </Form.Item>
+          <Form.Item name="date" label="Filter By Date:" className="mb-0">
+            <RangePicker
+              size="small"
+              format={{ format: "DD-MM-YYYY" }}
+              maxDate={dayjs(new Date())}
+            />
+          </Form.Item>
+        </Form>
+      }
     >
-      <Form.Item name="location" label="Filter By Location:" className="mb-0">
-        <Select
-          options={locations.map(({ id, name }) => ({
-            value: id,
-            label: name,
-          }))}
-          placeholder="Select a location"
-          allowClear
-          className="w-48"
-          size="small"
-        />
-      </Form.Item>
-      <Form.Item name="topic" label="Filter By Topic:" className="mb-0">
-        <Select
-          options={topics.map(({ id, name }) => ({
-            value: id,
-            label: name,
-          }))}
-          placeholder="Select a topic"
-          allowClear
-          className="w-48"
-          size="small"
-        />
-      </Form.Item>
-      <Form.Item name="status" label="Filter By Status:" className="mb-0">
-        <Select
-          options={statuses.map(({ id, name }) => ({
-            value: id,
-            label: name,
-          }))}
-          placeholder="Select a status"
-          allowClear
-          className="w-48"
-          size="small"
-        />
-      </Form.Item>
-      <Form.Item name="date" label="Filter By Date:" className="mb-0">
-        <RangePicker
-          size="small"
-          format={{ format: "DD-MM-YYYY" }}
-          maxDate={dayjs(new Date())}
-        />
-      </Form.Item>
-    </Form>
+      <Button
+        color="primary"
+        variant="outlined"
+        icon={<FaAngleDown />}
+        iconPosition="end"
+      >
+        Filters
+      </Button>
+    </Popover>
   );
 };
 
