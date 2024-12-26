fbd24512a45216e928f6de3203a57a96da971aeb
baadillahnabil <baadillahnabil@gmail.com>
2024-12-26 16:42:49 +0700

    feat: add getLocations and getTopics functions with type parameter support

--- src/app/_actions/actions.ts
+++ src/app/_actions/actions.ts
@@ -3,7 +3,9 @@
 import prisma from "@/app/_utils/prisma";
 import { type PromiseReturnType } from "@prisma/client/extension";
 
-export const getStatuses = async (type: "ids" | "names" | "both" = "both") => {
+type GetDataDefaultParams = "ids" | "names" | "both";
+
+export const getStatuses = async (type: GetDataDefaultParams = "both") => {
   const statuses = await prisma.status.findMany({
     select: {
       id: type === "ids" || type === "both",
@@ -15,3 +17,29 @@ export const getStatuses = async (type: "ids" | "names" | "both" = "both") => {
 };
 
 export type GetStatusesType = PromiseReturnType<typeof getStatuses>;
+
+export const getLocations = async (type: GetDataDefaultParams = "both") => {
+  const locations = await prisma.location.findMany({
+    select: {
+      id: type === "ids" || type === "both",
+      name: type === "names" || type === "both",
+    },
+  });
+
+  return locations;
+};
+
+export type GetLocationsType = PromiseReturnType<typeof getLocations>;
+
+export const getTopics = async (type: GetDataDefaultParams = "both") => {
+  const topics = await prisma.topic.findMany({
+    select: {
+      id: type === "ids" || type === "both",
+      name: type === "names" || type === "both",
+    },
+  });
+
+  return topics;
+};
+
+export type GetTopicsType = PromiseReturnType<typeof getTopics>;
