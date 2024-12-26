3a6e80701c87a7410553f803f5ede5657cbac151
baadillahnabil <baadillahnabil@gmail.com>
2024-12-26 13:32:54 +0700

    feat: add additional color variables to theme configuration

--- src/app/_styles/theme.ts
+++ src/app/_styles/theme.ts
@@ -5,5 +5,9 @@ export const theme: ThemeConfig = {
     colorBgLayout: "#F8F8F8",
     colorPrimary: "#1890FF",
     colorBgContainer: "#FFFFFF",
+    blue10: "#2989FF",
+    purple10: "#D787FF",
+    orange10: "#F18E57",
+    blue9: "#28CCCC",
   },
 };
